706e1149c17026a0d75cbf8a64b995095e30d58c
Sandun Sanjeewa <Sanjeewasandun82@gmail.com>
2024-08-08 15:36:39 +0530

    Initial commit

---
+++ client/src/getuser/User.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import "./user.css"
+
+const User = () => {
+    const [users,setUsers]=useState([])
+  return (
+    <div className='userTable'>
+        <button type="button" class="btn btn-primary">Add User <i class="fa-solid fa-user-plus"></i></button>
+        <table className='table table-bordered'>
+            <thead>
+                <tr>
+                    <th scope='col'>S.No</th>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>E-mail</th>
+                    <th scope='col'>Address</th>
+                    <th scope='col'>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>John</td>
+                    <td>John@gmail.com</td>
+                    <td>Canada</td>
+                    <td className='actionButtons'>
+                    
+                    <button type="button" class="btn btn-info"> <i class="fa-solid fa-pen-to-square"> </i> </button>
+                    <button type="button" class="btn btn-danger"> <i class="fa-solid fa-trash"> </i> </button>
+                    
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default User
