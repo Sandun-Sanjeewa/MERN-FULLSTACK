723fb46a0ca8aa7c44e73f226f3cbd530471cef4
Sandun Sanjeewa <Sanjeewasandun82@gmail.com>
2024-08-09 08:39:42 +0530

    Create Add User Form

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
     },
     {
       path:"/add",
-      element: <AddUser/>
+      element: <AddUser/>,
     }
   ])
   return (
--- client/src/adduser/AddUser.jsx
+++ client/src/adduser/AddUser.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
-
+import "./adduser.css"
+import { Link } from 'react-router-dom';
 const AddUser = () => {
   return (
-    <div>AddUser Form</div>
+    <div className='addUser'>
+        <Link to="/" type="button" class="btn btn-secondary">
+        <i class="fa-solid fa-backward"></i> Back
+        </Link>
+        <h3>Add New Users</h3>
+        <form className='addUserForm'>
+            <div className='inputGroup'>
+                <label htmlFor="name">Name:</label>
+                <input 
+                type='text'
+                id='name'
+                name='name'
+                autoComplete='off'
+                placeholder='Enter your name'/>
+            </div>
+            <div className='inputGroup'>
+                <label htmlFor="email">E-mail:</label>
+                <input 
+                type='email'
+                id='email'
+                name='email'
+                autoComplete='off'
+                placeholder='Enter your email'/>
+            </div>
+            <div className='inputGroup'>
+                <label htmlFor="address">Address:</label>
+                <input 
+                type='text'
+                id='address'
+                name='address'
+                autoComplete='off'
+                placeholder='Enter your Address'/>
+            </div>
+            <div className='inputGroup'>
+            <button type="button" class="btn btn-primary">
+                Submit
+            </button>
+            </div>
+        </form>
+
+    </div>
+    
   )
 };
 
