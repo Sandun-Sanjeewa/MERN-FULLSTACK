7ae70fedf638f6fb18ff6286459a1449fb3a2e5c
Sandun Sanjeewa <Sanjeewasandun82@gmail.com>
2024-08-09 07:03:50 +0530

    Display Data using Axios GET

--- client/src/getuser/User.jsx
+++ client/src/getuser/User.jsx
@@ -1,8 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./user.css"
+import axios from "axios"
 
 const User = () => {
+
+
     const [users,setUsers]=useState([])
+    useEffect(()=>{
+        const fetchData = async()=>{
+            try {
+                const response = await axios.get("http://localhost:8000/api/users")
+                setUsers(response.data)
+            } catch (error) {
+                console.log("Error while fetching data",error) 
+            }
+        };
+        fetchData();
+    },[]);
+
+
+
   return (
     <div className='userTable'>
         <button type="button" class="btn btn-primary">Add User <i class="fa-solid fa-user-plus"></i></button>
@@ -17,18 +34,22 @@ const User = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>John</td>
-                    <td>John@gmail.com</td>
-                    <td>Canada</td>
-                    <td className='actionButtons'>
-                    
-                    <button type="button" class="btn btn-info"> <i class="fa-solid fa-pen-to-square"> </i> </button>
-                    <button type="button" class="btn btn-danger"> <i class="fa-solid fa-trash"> </i> </button>
-                    
-                    </td>
-                </tr>
+                {users.map((user, index)=>{
+                    return(
+                        <tr>
+                        <td>{index+1}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.address}</td>
+                        <td className='actionButtons'>
+                        
+                        <button type="button" class="btn btn-info"> <i class="fa-solid fa-pen-to-square"> </i> </button>
+                        <button type="button" class="btn btn-danger"> <i class="fa-solid fa-trash"> </i> </button>
+                        
+                        </td>
+                    </tr>
+                    )
+                })}
             </tbody>
         </table>
     </div>
